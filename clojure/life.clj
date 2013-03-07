3ac0e45133de0467b225e4cbce5ddfd116d6290c
mishadoff <mishadoff@gmail.com>
2013-03-07 17:57:13 +0200

    Game of Life finished

--- clojure/life.clj
+++ clojure/life.clj
@@ -94,3 +94,67 @@
 
 ;; 137
 ;; Achievment unlocked!
+
+;; readability
+
+(defn step-sugar [grid range]
+  (let [count-neighbours ;; determine number of neighbours
+        (fn[[i j]]
+          (reduce + (map #(get-in grid % 0)
+                         (for[a [-1 0 1] b [-1 0 1]]
+                           [(+ i a) (+ j b)]))))
+        new-value  ;; calculate new value for cell
+        (fn [v [i j]]
+          (let [c (- (count-neighbours [i j]) v)]
+            (cond (= 3 c) 1
+                  (= 2 c) v
+                  :else 0)))
+        evolve-cell ;; update cell in a grid
+        (fn[g i] (update-in g i #(new-value % i )))]
+    (reduce evolve-cell grid range)))
+
+;; Usage
+
+(defn evolution [g]
+  (let [p (count g) q (count (get g 0)) ;; dimensions of grid
+        range (for [i (range p) j (range q)][i j])] ;; update whole grid
+    (iterate #(step7 % range) g)))
+
+(defn print-grid [g]
+  (doseq [s (map #(apply str (replace {0 "." 1 "⚫"} %)) g)]
+    (println s)))
+
+;; TESTING
+
+(defn run [grid]
+  (doseq [g (evolution grid)]
+    (print-grid g)
+    (Thread/sleep 1000)))
+
+(def block [[0 0 0 0]
+            [0 1 1 0]
+            [0 1 1 0]
+            [0 0 0 0]])
+
+(def blinker [[0 0 0]
+              [1 1 1]
+              [0 0 0]])
+
+(def toad [[0 0 0 0 0 0]
+           [0 0 0 0 0 0]
+           [0 0 1 1 1 0]
+           [0 1 1 1 0 0]
+           [0 0 0 0 0 0]
+           [0 0 0 0 0 0]])
+
+(def beacon [[1 1 0 0]
+             [1 1 0 0]
+             [0 0 1 1]
+             [0 0 1 1]])
+
+(def glider [[0 0 1 0 0 0 0 0]
+             [1 0 1 0 0 0 0 0]
+             [0 1 1 0 0 0 0 0]
+             [0 0 0 0 0 0 0 0]
+             [0 0 0 0 0 0 0 0]
+             [0 0 0 0 0 0 0 0]])
