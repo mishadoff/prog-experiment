0c61f74c5f44d978ddcb6a4093eee64e17bda726
mishadoff <mishadoff@gmail.com>
2013-08-09 21:36:00 +0300

    Brainfuck update

--- clojure/brainfuck.clj
+++ clojure/brainfuck.clj
@@ -16,14 +16,27 @@
   (case c
     \> (swap! p inc)
     \< (swap! p dec)
-    \+ (assoc! cs @p (inc (nth cs @p))) 
-    \- (assoc! cs @p (dec (nth cs @p)))
+    \+ (aset cs @p (inc (nth cs @p))) 
+    \- (aset cs @p (dec (nth cs @p)))
     \. (print (char (nth cs @p)))
-    \, (assoc! cs @p (.read *in*))
+    \, (aset cs @p (.read *in*))
 ))
 
 (defn parse [s]
-  (let [cs (transient (vec (repeat 100 0))) p (atom 0)]
+  (let [cs (int-array 100) p (atom 0)]
     (doseq [c s]
       (act c cs p))
-    (print (persistent! cs))))
+    (print cs)))
+
+(def parse-small
+
+(fn[s](let[a(int-array 100)p(atom 0)u #(aset a@p%)e #(nth a@p)x #(swap! p%)](doseq[c s](case c\>(x inc)\<(x dec)\+(u(inc(e)))\-(u(dec(e)))\.(print(char(e)))\,(u(.read *in*))))))
+                    ; ^
+                    ; ^  
+                    ; ^ 146 
+
+;; 177 characters
+;; -1(2) size of cells
+
+)
+
