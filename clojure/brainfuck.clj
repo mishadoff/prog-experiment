f59af7ede472ffa296049a0d419eafbb88074bbc
mishadoff <mishadoff@gmail.com>
2013-08-10 13:20:34 +0300

    Full Implementation of Brainfuck Interpreter. Starting Code Golf!

--- clojure/brainfuck.clj
+++ clojure/brainfuck.clj
@@ -12,31 +12,49 @@
 ;; [         start loop
 ;; ]         finish loop
 
-(defn act [c cs p ]
-  (case c
+(declare loop-)
+
+(defn act [a cs p i]
+  (case (nth cs @i)
     \> (swap! p inc)
     \< (swap! p dec)
-    \+ (aset cs @p (inc (nth cs @p))) 
-    \- (aset cs @p (dec (nth cs @p)))
-    \. (print (char (nth cs @p)))
-    \, (aset cs @p (.read *in*))
+    \+ (aset a @p (inc (nth a @p))) 
+    \- (aset a @p (dec (nth a @p)))
+    \. (print (char (nth a @p)))
+    \, (aset a @p (.read *in*))
+    \[ (if (zero? (nth a @p)) (loop- cs i inc dec))
+    \] (if-not (zero? (nth a @p)) (loop- cs i dec inc))
 ))
 
-(defn parse [s]
-  (let [cs (int-array 100) p (atom 0)]
-    (doseq [c s]
-      (act c cs p))
-    (print cs)))
+(defn loop- [cs i f g]
+  (loop [level 1]
+   (when-not (zero? level)
+     (swap! i f)
+     (case (nth cs @i)
+       \] (recur (g level))
+       \[ (recur (f level))
+       (recur level)))))
 
+(defn parse [s]
+  (let [a (int-array 100) p (atom 0)
+        cs (vec (seq s)) n (count cs)]
+    (loop [i (atom 0)]
+      (act a cs p i)
+      (swap! i inc) ;;
+      (if-not (= n @i) (recur i)))))
+           
+           
 (def parse-small
 
-(fn[s](let[a(int-array 100)p(atom 0)u #(aset a@p%)e #(nth a@p)x #(swap! p%)](doseq[c s](case c\>(x inc)\<(x dec)\+(u(inc(e)))\-(u(dec(e)))\.(print(char(e)))\,(u(.read *in*))))))
-                    ; ^
-                    ; ^  
-                    ; ^ 146 
+(fn[s](let[a(int-array 100)p(atom 0)c(vec(seq s))n(count c)
+l(fn[i f g](loop[level 1](when-not(zero? level)(swap! i f)(case(nth s@i)
+\](recur(g level))\[ (recur (f level))(recur level)))))]
+(loop[i(atom 0)](case (nth c @i)\>(swap! p inc)\< (swap! p dec)
+\+(aset a@p(inc(nth a@p)))\-(aset a@p(dec(nth a@p)))\.(print(char(nth a@p)))
+\,(aset a@p(.read *in*))\[(if(zero?(nth a@p))(l i inc dec))
+\](if-not(zero?(nth a@p))(l i dec inc)))(swap! i inc)(if-not(= n @i)(recur i)))))
 
-;; 177 characters
-;; -1(2) size of cells
+;; 472 characters
 
 )
 
