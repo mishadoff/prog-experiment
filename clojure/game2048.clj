8029a1b9fab5bc8b61ac53a19fbf9568c5a7151a
mishadoff <mishadoff@gmail.com>
2014-03-14 03:15:50 +0200

    Game2048 > Folding

--- clojure/game2048.clj
+++ clojure/game2048.clj
@@ -7,15 +7,37 @@
                 (repeat SIZE)
                 (vec)))
 
-(defn empty-cells [grid]
+(defn- empty-cells [grid]
   (filter
    #(zero? (get-in grid %))
    (for [i (range SIZE) j (range SIZE)] [i j])))
 
-(defn spawn [grid]
+(defn- spawn [grid]
   (let [ec (empty-cells grid)]
     (if (< (count ec) 2) (throw (RuntimeException. "Game Over")))
     (reduce #(update-in %1 %2 + 2) grid (take 2 (shuffle ec)))))
 
-(defn move [grid]
-  "TODO")
+(defn- fold [line]
+  (->> (remove zero? line)
+       (partition-by identity)
+       (map (fn [[p & _ :as part]]
+              (condp = (count part)
+                1 p 
+                2 (* 2 p)
+                3 [(* 2 p) p]
+                4 [(* 2 p) (* 2 p)])))
+       (flatten)
+       (#(concat % (repeat (- 4 (count %)) 0)))
+       (vec)))
+
+(defn- move-left [grid]
+  (mapv fold grid))
+
+(defn- move-down [grid]
+  )
+
+;; Visualisation
+
+(defn- printg [grid]
+  (doseq [line grid]
+    (println line)))
